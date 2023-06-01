19bac49776102846fd87933a07e878c73aa2f655
Gautam Singh <goats.singh@gmail.com>
2023-06-01 08:57:39 +0000

    Change approach for this experiment

--- avr-gcc/decade/main.c
+++ avr-gcc/decade/main.c
@@ -1,17 +1,4 @@
-#include <avr/io.h> 
-#include <util/delay.h> 
-#include <stdlib.h> 
-
-// Declare assembly implemented routines
-extern void LCD_Init(void);
-extern void LCD_Cmd(uint8_t);
-extern void LCD_Char(uint8_t);
-extern void LCD_SetCursor(uint8_t, uint8_t);
-extern void LCD_Clear(void);
-
-void LCD_String(const char *str) {
-	while (*str) LCD_Char(*str++);
-}
+#include "lcd.h"
 
 int main(void) {
 	LCD_Init(); // initialize LCD controller
@@ -31,7 +18,7 @@ int main(void) {
 		for (int i = 0; i <= 9; i++) {
 			LCD_Clear();
 			LCD_String(digits[i]);
-			LCD_SetCursor(1,0);
+			LCD_Cmd(0xC0);
 			LCD_Char('0'+i);
 			_delay_ms(1000);
 		}
