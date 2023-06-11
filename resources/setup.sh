e40c69f852f0e6b2ac67c3888ade784204f4b2db
Gautam Singh <goats.singh@gmail.com>
2023-06-11 13:19:47 +0000

    Change setup script and add fpga codes

---
+++ resources/setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#Update package repos
+apt update -y && apt upgrade -y
+
+#Install new packages
+apt install openssh-server sshpass build-essential libssl-dev libffi-dev python3-dev bison flex git-all git-lfs hub tcl-dev tcl tcl-tclreadline libreadline-dev  autoconf libtool make automake texinfo pkg-config libusb-1.0-0 libusb-1.0-0-dev gcc-arm-none-eabi libnewlib-arm-none-eabi telnet python3 apt-utils libxslt-dev cmake curl python3-pip python3-venv -y
+
+#Setup git LFS
+git lfs install
+
+#Create a python3 virtual environment for installed modules
+python3 -m venv ~/.pyvenv
+
+#Create directory to install the toolchain to
+mkdir -p /root/symbiflow
+echo "export INSTALL_DIR=/root/symbiflow" >> ~/.pyvenv/bin/activate
+echo "export PATH=$INSTALL_DIR/quicklogic-arch-defs/bin:$INSTALL_DIR/quicklogic-arch-defs/bin/python3:$PATH" >> ~/.pyvenv/bin/activate
+echo "export PATH=/data/data/com.termux/files/home/symbiflow/bin:$PATH" >> ~/.pyvenv/bin/activate
+cd
+
+source ~/.pyvenv/bin/activate
+
+#Clone pygmy toolchain
+git clone --recursive https://github.com/optimuslogic/pygmy-dev
+
+#Download symbiflow and patches
+git clone https://github.com/goats-9/fwc-codes
+cp fwc-codes/resources/arch.tar.gz .
+cp fwc-codes/resources/quicklogic-fasm.patch pygmy-dev/tools/quicklogic-fasm
+cp fwc-codes/resources/quicklogic-yosys.patch pygmy-dev/tools/quicklogic-yosys
+cp fwc-codes/resources/vtr-verilog-to-routing.patch pygmy-dev/tools/vtr-verilog-to-routing
+
+rm -rf fwc-codes
+tar -C $INSTALL_DIR -xvf arch.tar.gz
+
+#Install fasm
+cd ~/pygmy-dev/tools/quicklogic-fasm
+git apply quicklogic-fasm.patch
+pip3 install -r requirements.txt
+python3 setup.py install
+
+#Install yosys
+cd ~/pygmy-dev/tools/quicklogic-yosys
+git apply quicklogic-yosys.patch
+make config-gcc
+make -j4 install PREFIX=$INSTALL_DIR
+
+#Install yosys symbiflow plugins
+cd ~/pygmy-dev/tools/yosys-symbiflow-plugins
+make -j4 install
+
+#Install vtr
+cd ~/pygmy-dev/tools/vtr-verilog-to-routing
+git apply vtr-verilog-to-routing.patch
+make -j4
+
+#Copy executables onto PATH
+cp build/vpr/vpr $INSTALL_DIR/bin/
+cp build/utils/fasm/genfasm $INSTALL_DIR/bin/
--- vaman/fpga/lcd/lcd.v
+++ vaman/fpga/lcd/lcd.v
@@ -1,88 +1,143 @@
-`timescale 1us/1us
-
-module lcd_adder ( 
-    output reg [7:4] data,
-    output reg en,
+module lcd (
     output reg rs,
-    input [3:0] a,
-    input [3:0] b,
+    output reg en,
+    output reg[7:4] data,
 );
 
     wire clk;
-    integer c, d, e;
     qlal4s3b_cell_macro u_qlal4s3b_cell_macro (
     	.Sys_Clk0 (clk),
     );
 
-    // Task to pulse enable line to clock
-    // 4 bits into microcontroller.
-    task PulseEnableLine;
-        begin
-            en <= 1;
-            #50 en <= 0;
-        end
-    endtask
+    integer i = 1;
+    integer count = 0;
+    reg [3:0] nibs [1:51];
+    integer a = 9, b = 10;
+    reg [7:0] c = a + b, c1, c0, o, a1, a0, b1, b0;
 
-    // Task to send byte to LCD
-    // lcd_rs = 0 -> command byte
-    // lcd_rs = 1 -> data byte
-    task SendByte;
-        input lcd_rs;
-        input [7:0] byte;
-        begin
-            rs <= lcd_rs;
-            data[7:4] <= byte[7:4];
-            PulseEnableLine();
-            data[7:4] <= byte[3:0];
-        end
-    endtask
-
-    // Task to output an integer
-    // on LCD
-    task SendInteger;
-        input integer res;
-        integer num, digit;
-        begin
-            num = res;
-            while (num) begin
-            //for (num = res; num > 0; num = num / 10) begin
-                digit = num % 10;
-                SendByte(1, 48 + digit);
-            end
-        end
-    endtask
-
-    // Initialize the LCD
     initial begin
-        SendByte(0, 8'h33);
-        SendByte(0, 8'h32);
-        SendByte(0, 8'h28);
-        SendByte(0, 8'h0C);
-        SendByte(0, 8'h06);
-        SendByte(0, 8'h01);
-        #3000;
+        if (c >= 16) begin
+            c = c % 16;
+            o = 49;
+        end
+        else begin
+            o = 48;
+        end
+        c0 = (c % 10) + 48;
+        c1 = (c / 10) + 48;
+        a0 = (a % 10) + 48;
+        a1 = (a / 10) + 48;
+        b0 = (b % 10) + 48;
+        b1 = (b / 10) + 48;
     end
 
     always @(posedge clk) begin
-        SendByte(0, 8'h01);
-        // Do computation
-        #3000 c = a+b;
-        // Send answer to LCD
-        d = c % 16;
-        e = d % 10;
-        d = d / 10;
-        SendByte(1, 48+e);
-        SendByte(1, 48+d);
-        if (c > 15) begin
-            SendByte(0, 8'h80);
-            SendByte(1, "O");
-            SendByte(1, "v");
-            SendByte(1, "e");
-            SendByte(1, "r");
-            SendByte(1, "f");
-            SendByte(1, "l");
-            SendByte(1, "o");
-            SendByte(1, "w");
+        nibs[1]  = 4'h3;
+        nibs[2]  = 4'h3;
+        nibs[3]  = 4'h3;
+        nibs[4]  = 4'h2;
+        nibs[5]  = 4'h2;
+        nibs[6]  = 4'h8;
+        nibs[7]  = 4'h0;
+        nibs[8]  = 4'hC;
+        nibs[9]  = 4'h0;
+        nibs[10] = 4'h6;
+        nibs[11] = 4'h0;
+        nibs[12] = 4'h1;
+        nibs[13] = 4'h8;//Start here
+        nibs[14] = 4'h0;
+        nibs[15] = 1'b1;
+        nibs[16] = 4'h6;
+        nibs[17] = 4'h1;//'a'
+        nibs[18] = 4'h3;
+        nibs[19] = 4'hD;//'='
+        nibs[20] = a1[7:4];
+        nibs[21] = a1[3:0];//a1
+        nibs[22] = a0[7:4];
+        nibs[23] = a0[3:0];//a0
+        nibs[24] = 4'h2;
+        nibs[25] = 4'h0;//' '
+        nibs[26] = 4'h6;
+        nibs[27] = 4'h2;//'b';
+        nibs[28] = 4'h3;
+        nibs[29] = 4'hD;//'='
+        nibs[30] = b1[7:4];
+        nibs[31] = b1[3:0];//b1
+        nibs[32] = b0[7:4];
+        nibs[33] = b0[3:0];//b0
+        nibs[34] = 4'hC;
+        nibs[35] = 4'h0;
+        nibs[36] = 4'h6;
+        nibs[37] = 4'h3;//'c'
+        nibs[38] = 4'h3;
+        nibs[39] = 4'hD;//'='
+        nibs[40] = c1[7:4];
+        nibs[41] = c1[3:0];//c1
+        nibs[42] = c0[7:4];
+        nibs[43] = c0[3:0];//c0
+        nibs[44] = 4'h2;
+        nibs[45] = 4'h0;//' '
+        nibs[46] = 4'h6;
+        nibs[47] = 4'hF;//'o'
+        nibs[48] = 4'h3;
+        nibs[49] = 4'hD;//'='
+        nibs[50] = o[7:4];
+        nibs[51] = o[3:0];//o
+    end
+    always @(posedge clk) begin
+        if (i<15)
+        begin
+        	rs<=1'b0;
+        	data=nibs[i];
+        	en<=1'b1;
+        	if (count==800)
+            begin
+           		en<=1'b0;
+           		count<=0;
+           		i <= i + 1;
+           	end
+        	else
+           		count<=count+1;
         end
+        if (i==15) 
+        begin
+        	if (count==60000)
+            begin
+             	count<=0;
+             	i<=i+1;
+            end
+        	else
+             	count<=count+1;	
+        end
+        if ((i>15 && i<34) || (i>35 && i <= 51)) 
+        begin 
+        	rs<=1'b1;
+        	data=nibs[i];
+            en<=1'b1;
+        	if (count==800)
+            begin
+        	    en<=1'b0;
+        	    count<=0;
+                i <= i + 1;
+            end
+            else
+        		count<=count+1;       
+        end
+        if (i>=34 && i<=35)
+        begin
+            rs <= 1'b0;
+            data = nibs[i];
+            en<=1'b1;
+        	if (count==800)
+            begin
+        	    en<=1'b0;
+        	    count<=0;
+                i <= i + 1;
+            end
+            else
+        		count<=count+1;       
+        end
+        if (i>51) 
+        	i<=13;
     end
 endmodule
