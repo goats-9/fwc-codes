cca64281a069ad960d72edb06b02df463e85ece4
Gautam Singh <goats.singh@gmail.com>
2023-06-19 13:42:58 +0530

    Add new data

--- tiny-ml/data/format.c
+++ tiny-ml/data/format.c
@@ -2,42 +2,42 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define NUM_SAMPLES 20
+#define NUM_SAMPLES 30
 #define NUM_GAP 10
-#define NUM_GESTURES 30
+#define NUM_GESTURES 100
 #define ACC_THR 30.0
 
 FILE *dst;
 
 void process(FILE *afile, FILE *gfile, int tgt) { 
-    char h1[5], h2[5], h3[5], h4[5];
-    fscanf(afile, "%s %s %s %s", h1, h2, h3, h4);
-    fscanf(gfile, "%s %s %s %s", h1, h2, h3, h4);
-    double time, ax, ay, az, gx, gy, gz;
+    char h1[100];
+    fscanf(afile, "%s", h1);
+    fscanf(gfile, "%s", h1);
+    double time, el, ax, ay, az, gx, gy, gz;
     int ng = 0;
-    while (ng < NUM_GESTURES && fscanf(afile, "%lf %lf %lf %lf", &time, &ax, &ay, &az) != -1) {
-        fscanf(gfile, "%lf %lf %lf %lf", &time, &gx, &gy, &gz);
+    while (ng < NUM_GESTURES && fscanf(afile, "%lf,%lf,%lf,%lf,%lf", &time, &el, &az, &ay, &ax) != -1) {
+        fscanf(gfile, "%lf,%lf,%lf,%lf,%lf", &time, &el, &gz, &gy, &gx);
         if (fabs(ax) + fabs(ay) + fabs(az) < ACC_THR) continue;
         fprintf(dst, "%lf,%lf,%lf,%lf,%lf,%lf,", ax, ay, az, gx, gy, gz);
         for (int i = 1; i < NUM_SAMPLES; i++) {
-            fscanf(afile, "%lf %lf %lf %lf", &time, &gx, &gy, &gz);
-            fscanf(gfile, "%lf %lf %lf %lf", &time, &gx, &gy, &gz);
+            fscanf(afile, "%lf,%lf,%lf,%lf,%lf", &time, &el, &az, &ay, &ax);
+            fscanf(gfile, "%lf,%lf,%lf,%lf,%lf", &time, &el, &gz, &gy, &gx);
             fprintf(dst, "%lf,%lf,%lf,%lf,%lf,%lf,", ax, ay, az, gx, gy, gz);
         }
         fprintf(dst, "%d\n", tgt);
         for (int i = 0; i < NUM_GAP; i++) {
-            fscanf(afile, "%lf %lf %lf %lf", &time, &gx, &gy, &gz);
-            fscanf(gfile, "%lf %lf %lf %lf", &time, &gx, &gy, &gz);
+            fscanf(afile, "%lf,%lf,%lf,%lf,%lf", &time, &el, &az, &ay, &ax);
+            fscanf(gfile, "%lf,%lf,%lf,%lf,%lf", &time, &el, &gz, &gy, &gx);
         }
         ++ng;
     }
 }
 
 int main() {
-    FILE *ach = fopen("acce_chop.txt", "r");
-    FILE *gch = fopen("gyro_chop.txt", "r");
-    FILE *awr = fopen("acce_wrist.txt", "r");
-    FILE *gwr = fopen("gyro_wrist.txt", "r");
+    FILE *ach = fopen("acce_chop.csv", "r");
+    FILE *gch = fopen("gyro_chop.csv", "r");
+    FILE *awr = fopen("acce_wrist.csv", "r");
+    FILE *gwr = fopen("gyro_wrist.csv", "r");
     dst = fopen("data.csv", "w");
     for(int i = 0; i < NUM_SAMPLES; i++) {
         fprintf(dst, "ax%d,ay%d,az%d,gx%d,gy%d,gz%d,", i, i, i, i, i, i);
--- tiny-ml/data/preprocess.py
+++
@@ -1,8 +0,0 @@
-for file in ['acce_chop.txt', 'acce_wrist.txt', 'gyro_chop.txt', 'gyro_wrist.txt']:
-	fh = open(file, 'r')
-	L = fh.readlines()
-	fh.close()
-	fh1 = open(file, 'w')
-	for line in L:
-		fh1.write(line.strip() + '\n')
-	fh1.close()
